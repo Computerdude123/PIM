bd5fa4103d0f9eca44ee5bf306273d57a0661db0
Computerdude123 <tudor.burlacu06@gmail.com>
2023-05-15 15:00:06 +0300

    0.2

--- src/User.java
+++ src/User.java
@@ -1,3 +1,7 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class User {
@@ -15,12 +19,37 @@ public class User {
     public String getUsername() {
         return username;
     }
-    private ArrayList<User> users = new ArrayList<>();
+    static ArrayList<User> users = new ArrayList<>();
 
-    public ArrayList<User> getUsers() {
-        return users;
+    public boolean searchUsers(User u){
+        if ()
     }
-    public boolean searchUsers(String username, String password){
-        return (users.contains(username) && users.contains(password));
+    public void addUsers(User user){
+        users.add(user);
+        try {
+            FileWriter mywriter = new FileWriter("output.txt");
+            for (int i = 0; i < users.size(); i++) {
+                mywriter.write(users.get(i).username + System.lineSeparator());
+                mywriter.write(users.get(i).password);
+            }
+            mywriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void setUsers() {
+        ArrayList<String> lines = null;
+        try {
+            lines = new ArrayList<>(Files.readAllLines(Paths.get("output.txt")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < lines.size(); i++) {
+            String username = lines.get(i);
+            String password = lines.get(i + 1);
+            User u1 = new User(username,password);
+            users.add(u1);
+            i++;
+        }
     }
 }
