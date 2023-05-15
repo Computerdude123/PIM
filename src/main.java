b6b49f6e5826bfb8f65295b26f325fded5c14567
Computerdude123 <tudor.burlacu06@gmail.com>
2023-05-15 14:59:29 +0300

    0.2

--- src/main.java
+++ src/main.java
@@ -3,22 +3,20 @@ import java.util.Scanner;
 public class main {
     static Scanner scanner = new Scanner(System.in);
     static Scanner scanner2 = new Scanner(System.in);
-    static User u = new User("a","a");
     public static void main(String[] args) {
+        User.setUsers();
         int o = 0;
         while (o != 3){
             System.out.println("""
                     Photo Identification Model!!!
-                    
                     1. Log In
                     2. Register
-                    3. Exit
-                    """);
+                    3. Exit""");
             o = scanner.nextInt();
             if (o == 1){
                 LogIn();
             }else if(o == 2){
-
+                Register();
             }
         }
     }
@@ -27,12 +25,21 @@ public class main {
         String username = scanner2.nextLine();
         System.out.println("Password:");
         String password = scanner2.nextLine();
-        if(!u.searchUsers(username, password)){
+        User u = new User(username, password);
+        if(u.searchUsers(u)){
+            Menu();
+        }else {
             System.out.println("Username or password is wrong! Try Again!");
             LogIn();
-        }else {
-            Menu();
         }
+    }
+    public static void Register(){
+        System.out.println("Username:");
+        String username = scanner2.nextLine();
+        System.out.println("Password:");
+        String password = scanner2.nextLine();
+        User u1 = new User(username, password);
+        u1.addUsers(u1);
     }
     public static void Menu(){
         System.out.println("Add image:");
